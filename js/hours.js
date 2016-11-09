b46a600bb13e0d0ba61b9ae368c813e701731131
Alice Frumin <alice@aghstrategies.com>
2016-11-09 15:38:28 -0600

    using crm entity ref to get volunteer need select

--- js/hours.js
+++ js/hours.js
@@ -2,23 +2,18 @@ CRM.$(function ($) {
   var setUpNeedSelect = function () {
       $('#needSelectId').find('option').remove();
       var $volProjectId = $('#volunteer_project_select').val();
-      CRM.api3('VolunteerNeed', 'get', {
-        'project_id': $volProjectId,
-      }).done(function (result) {
-          var needs = result.values;
-          $.each(needs, function () {
-            var formattedText =  this.role_label + ', Time: ' + this.display_time;
-            $('<option />', { value: this.id, text: formattedText }).appendTo(needSelect);
-          });
-          $('<option />', { value: none, text: 'Need not listed' }).appendTo(needSelect);
-          $('#needSelectId').select2();
-
-        });
+  $('#needSelectId').crmEntityRef({
+	entity: 'VolunteerNeed',
+        api: { 
+          search_field: 'role_label',
+        },
+        create: true,
+      });
     };
 
   var txt1 = '<div class = "crm-section"><div class="label"><label for="volunteer_need_select">Volunteer Need <span class = "crm-marker" title="This Field is required.">*</span></label></div><div class="content jsselect"></div><div class="clear"></div><div>';
   $('div.crm-section:nth-child(6)').append(txt1);
-  var needSelect = $("<select id=\"needSelectId\" class=\"crm-form-select\" />");
+  var needSelect = $("<div name= \"projectSelect\" id=\"needSelectId\" class=\"crm-form-select\" />");
   needSelect.appendTo('.jsselect');
   setUpNeedSelect();
   $('#volunteer_project_select').change(setUpNeedSelect);
