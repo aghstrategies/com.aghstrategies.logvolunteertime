ceb70014bc0618829cc5ea7d1fe873a12b9dabad
Alice Frumin <alice@aghstrategies.com>
2016-11-09 14:18:01 -0600

    adding date to select 2

--- js/hours.js
+++ js/hours.js
@@ -7,13 +7,16 @@ CRM.$(function ($) {
       }).done(function (result) {
           var needs = result.values;
           $.each(needs, function () {
-            $('<option />', { value: this.id, text: this.role_label }).appendTo(needSelect);
+var formattedText =  this.role_label + ', ' + this.display_time;
+
+            $('<option />', { value: this.id, text: formattedText }).appendTo(needSelect);
           });
+$('#needSelectId').select2();
 
         });
     };
 
-  var txt1 = '<div class = "crm-section"><div class="label"><label for="volunteer_need_select">Volunteer Need</label></div><div class="content jsselect"></div><div class="clear"></div><div>';
+  var txt1 = '<div class = "crm-section"><div class="label"><label for="volunteer_need_select">Volunteer Need <span class = "crm-marker" title="This Field is required.">*</span></label></div><div class="content jsselect"></div><div class="clear"></div><div>';
   $('div.crm-section:nth-child(6)').append(txt1);
   var needSelect = $("<select id=\"needSelectId\" class=\"crm-form-select\" />");
   needSelect.appendTo('.jsselect');
