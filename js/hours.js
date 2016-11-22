a9553acb38765ec0f42bb92de0344a53985d5126
Alice Frumin <alice@aghstrategies.com>
2016-11-22 10:59:27 -0600

    updated code

--- js/hours.js
+++ js/hours.js
@@ -10,12 +10,13 @@ CRM.$(function ($) {
         },
         select: { minimumInputLength: 0 },
         create: true,
+        placeholder: 'Select a Need',
       });
     };
 
   var txt1 = '<div class = "crm-section"><div class="label"><label for="volunteer_need_select">Volunteer Need <span class = "crm-marker" title="This Field is required.">*</span></label></div><div class="content jsselect"></div><div class="clear"></div><div>';
   $('div.crm-section:nth-child(6)').append(txt1);
-  var $needSelect = $('<div name= \'projectSelect\' id=\'needSelectId\' class=\'crm-form-select\' />');
+  var $needSelect = $('<input type=\'hidden\' name= \'projectSelect\' id=\'needSelectId\' class=\'crm-form-select\' />');
   $needSelect.appendTo('.jsselect');
   setUpNeedSelect();
   $('#volunteer_project_select').change(setUpNeedSelect);
